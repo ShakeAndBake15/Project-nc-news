d0fd47482960068bcad011328f74fb192cee5066
Alex Lyon <alexlyon@Alexs-MacBook-Air.local>
2022-10-10 15:34:26 +0100

    console logs removed

--- server/apps.js
+++ server/apps.js
@@ -17,7 +17,6 @@ app.use((err, req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.log(err)
   if(err.code === "22P02") {
     res.status(400).send({msg: 'Incorrect request format'})
   } else {
